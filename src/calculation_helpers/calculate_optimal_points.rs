135914a520fce33a8d7f968821e483322489480a
Patrick Styer <pjstyer@gmail.com>
2024-12-13 09:42:44 -0800

    fix(bench-king): calculation corrections

--- src/calculation_helpers/calculate_optimal_points.rs
+++ src/calculation_helpers/calculate_optimal_points.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use crate::models::{
     matchup::Matchup,
@@ -32,7 +32,6 @@ pub fn optimal_score_for_matchup(
     players: Players,
     league_positions: Vec<RosterPosition>,
 ) -> OptimalScoreForMatchup {
-    //println!("Matchup Points: {:?}", matchup.points);
     let mut optimal_roster = OptimalScoreForMatchup {
         matchup_id: matchup.matchup_id,
         roster_id: roster.roster_id,
@@ -46,82 +45,83 @@ pub fn optimal_score_for_matchup(
             if pos == &RosterPosition::BN {
                 return acc;
             }
-            let count: &mut u8 = acc.entry(pos).or_insert(0);
-            *count += 1;
+            acc.entry(pos).and_modify(|c| *c += 1).or_insert(1);
             acc
         })
         .iter()
         .map(|(pos, count)| (**pos, *count))
         .collect::<Vec<(RosterPosition, u8)>>();
 
-    // Sort by the number of football positions a fantasy position can hold
-    roster_position_count.sort_by_key(|a| (a.0.value().len(), a.1));
-    //println!("Roster Position Count: {:?}", roster_position_count);
+    // Sort by the number of football positions a fantasy position can hold, fewest first.
+    // When slotting players, the highest point player should go into the most restrictive slot, because
+    // that allow flex spot to use more positions. In the case of a tie, put them in the one with the most
+    // slots, to allow a more restricted player to fit into the other slot.
+    roster_position_count.sort_by_key(|a| (a.0.value().len(), a.1 as i32 * -1));
 
     let viable_players_with_stats = players
         .iter()
         .filter(|(player_id, _)| matchup.players.iter().any(|p| &p == player_id))
         .map(|(_, details)| details)
         .collect::<Vec<&PlayerDetails>>();
-    //println!("Viable Players: {:?}", viable_players_with_stats.iter().map(|p| (p.full_name.as_ref().unwrap_or(&p.player_id), p.position.unwrap_or(RosterPosition::SuperFlex))).collect::<Vec<(&String, RosterPosition)>>());
 
-    let mut used_players = Vec::new();
+    let mut used_players = HashSet::new();
 
     for (position, count) in roster_position_count {
-        //println!("Drafting {} players for position {:?}", count, position);
+        // Any position the player plays is associated with any position the league slots.
         let mut players_for_position = viable_players_with_stats
             .iter()
             .filter(|&player| match &player.fantasy_positions {
                 Some(fp) => fp
                     .iter()
-                    .any(|p| position.value().iter().any(|v| p.value().contains(v))),
+                    .any(|p| position.value().iter().any(|v| p.value().contains(v) && matchup.players.contains(&player.player_id))),
                 None => false,
             })
             .map(|p| p.to_owned().to_owned())
             .collect::<Vec<PlayerDetails>>();
-        players_for_position.sort_by(|a, b| {
-            let a_pts = matchup.players_points.get(&a.player_id).unwrap_or(&0.0f32);
-            let b_pts = matchup.players_points.get(&b.player_id).unwrap_or(&0.0f32);
-            a_pts.partial_cmp(b_pts).unwrap()
+        // Sort the vec by points, highest _last_ so we can pop the players off later.
+        players_for_position.sort_by_key(|a| {
+            (matchup.players_points.get(&a.player_id).unwrap_or(&0.0f32) * 1000.0) as i32
         });
-        // println!("Players for position: {:?}", players_for_position.iter().map(|p| match &p.full_name {
-        //     Some(name) => (name.to_string(), matchup.players_points.get(&p.player_id).unwrap_or(&0.0f32)),
-        //     None => (p.player_id.to_string(), matchup.players_points.get(&p.player_id).unwrap_or(&0.0f32)),
-        // }).collect::<Vec<(String,&f32)>>());
+
         for _l in 0..count {
-            //println!("Looop count: {}, Player Count: {}, Position: {:?}", l, players_for_position.len(), position.value());
-            let (player, up, pfp) = get_player(&used_players, players_for_position);
-            used_players = [used_players, up].concat().clone();
+            if players_for_position.len() == 0 {
+                continue
+            }
+            let (player, up, pfp) = get_player(&mut used_players, players_for_position);
+            used_players = up.clone();
             players_for_position = pfp;
-            let player_id = &player.player_id;
-            let points = matchup.players_points.get(player_id).unwrap_or(&0.0f32);
-            //println!("Player: {:?}; Points: {}", player.full_name.as_ref().unwrap_or(&player.player_id), points);
+            let points = matchup.players_points.get(&player.player_id).unwrap_or(&0.0f32);
             optimal_roster.optimal_points += points;
-            used_players.push(player_id.to_string());
+            used_players.insert(player.player_id.to_string());
         }
     }
     optimal_roster
 }
 
 fn get_player(
-    used_players: &Vec<String>,
+    used_players: &mut HashSet<String>,
     mut players: Vec<PlayerDetails>,
-) -> (PlayerDetails, Vec<String>, Vec<PlayerDetails>) {
+) -> (PlayerDetails, &HashSet<String>, Vec<PlayerDetails>) {
+    // Take the last player. We sort ASC on the ppints, so this is the highest point player remaining.
     match players.pop() {
         Some(p) => {
-            if used_players.contains(&p.player_id) {
-                //println!("Player already used: {:?}", p.full_name.as_ref().unwrap_or(&p.player_id));
-                get_player(
-                    &[used_players.clone(), vec![p.player_id.clone()]].concat(),
-                    players.clone(),
-                );
-            }
-            (
-                p.clone().clone(),
-                [used_players.clone(), vec![p.player_id.clone()]].concat(),
-                players,
-            )
+            let is_used = used_players.contains(&p.player_id);
+            used_players.insert(p.player_id.clone());
+            if is_used {
+                return get_player(used_players, players)
         }
-        None => get_player(used_players, players),
+            (
+            p.clone().clone(),
+            used_players,
+            players,
+        )
     }
+        None => get_player(used_players, players)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_calculate_optimal_points() {}
 }
